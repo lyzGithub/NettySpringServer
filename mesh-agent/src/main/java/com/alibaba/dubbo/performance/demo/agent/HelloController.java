0b48c7277e4a171d9343f5dc2c01f6a800d62ceb
lyz <602298461@qq.com>
2018-06-07 21:44:34 +0800

    demo

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/HelloController.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/HelloController.java
@@ -49,13 +49,13 @@ public class HelloController {
 
     public byte[] provider(String interfaceName,String method,String parameterTypesString,String parameter) throws Exception {
 
-        System.out.println("provider: "+interfaceName+method +":" + parameterTypesString +":"+ parameter);
+        //System.out.println("provider: "+interfaceName+":"+method +":" + parameterTypesString +":"+ parameter);
         Object result = rpcClient.invoke(interfaceName,method,parameterTypesString,parameter);
         return (byte[]) result;
     }
 
     public Integer consumer(String interfaceName,String method,String parameterTypesString,String parameter) throws Exception {
-        System.out.println("provider: "+interfaceName+method +":" + parameterTypesString +":"+ parameter);
+        //System.out.println("provider: "+interfaceName+":"+method +":" + parameterTypesString +":"+ parameter);
         if (null == endpoints){
             synchronized (lock){
                 if (null == endpoints){
