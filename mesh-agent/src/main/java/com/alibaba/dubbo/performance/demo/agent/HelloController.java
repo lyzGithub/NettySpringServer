4ee51996bde627840bda25a5f94e24c4f5ed5fd0
lyz <602298461@qq.com>
2018-06-08 10:56:30 +0800

    ede

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/HelloController.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/HelloController.java
@@ -56,13 +56,13 @@ public class HelloController {
 
     public Integer consumer(String interfaceName,String method,String parameterTypesString,String parameter) throws Exception {
         //System.out.println("provider: "+interfaceName+":"+method +":" + parameterTypesString +":"+ parameter);
-        if (null == endpoints){
+        /*if (null == endpoints){
             synchronized (lock){
                 if (null == endpoints){
-                    endpoints = registry.find("com.alibaba.dubbo.performance.demo.provider.IHelloService");
                 }
             }
-        }
+        }*/
+        endpoints = registry.find("com.alibaba.dubbo.performance.demo.provider.IHelloService");
 
         int size = 0;
         size = endpoints.size();
