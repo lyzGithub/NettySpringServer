c614f66e3dc5dcbb2d71757e6b322a75a56c6454
lyz <602298461@qq.com>
2018-06-09 17:29:15 +0800

    ded

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/provider/client/ClientToProvider.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/provider/client/ClientToProvider.java
@@ -30,6 +30,11 @@ public class ClientToProvider {
         @Override
         public void run() {
             int i = 100;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             while(i>0) {
                 i--;
                 try {
