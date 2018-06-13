4c160c04d50078bcd88a1fe54bd6d9e1b20296c3
yize li <602298461@qq.com>
2018-06-13 15:32:46 +0800

    test dubbo

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/agent/server/ProviderAgentServiceImpl.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/agent/server/ProviderAgentServiceImpl.java
@@ -10,11 +10,11 @@ import org.slf4j.LoggerFactory;
 
 public class ProviderAgentServiceImpl implements ProviderAgentService{
 
-    //private RpcClient rpcClient ;
+    private RpcClient rpcClient ;
     private static Logger logger = LoggerFactory.getLogger(ProviderAgentServiceImpl.class);
 
     public ProviderAgentServiceImpl(){
-        //this.rpcClient = new RpcClient();
+        this.rpcClient = new RpcClient();
     }
 
     @Override
@@ -22,23 +22,23 @@ public class ProviderAgentServiceImpl implements ProviderAgentService{
         //logger.info("receive: " + para);
         return Integer.toString(para.hashCode());
     }
+
+    /*
     private final String interfaceName = "com.alibaba.dubbo.performance.demo.provider.IHelloService";
     private final String parameterTypesString = "Ljava/lang/String;";
-    private final String methodName = "hash";
+    private final String methodName = "hash";*/
     @Override
     public String getHashCode(RequestPara requestPara) {
 
         Object result = null;
         try {
-            /*result = rpcClient.invoke(requestPara.getInterfaceName(),requestPara.getMethodName(),
-                    requestPara.getParameterTypesString(),requestPara.getParameter());*/
-            /*result = rpcClient.invoke(interfaceName,methodName,
-                    parameterTypesString,requestPara.getParameter());*/
+            result = rpcClient.invoke(requestPara.getInterfaceName(),requestPara.getMethodName(),
+                    requestPara.getParameterTypesString(),requestPara.getParameter());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //String hashCode = new String((byte[]) result);
-        String hashCode = Integer.toString(requestPara.getParameter().hashCode());
+        String hashCode = new String((byte[]) result);
+        //String hashCode = Integer.toString(requestPara.getParameter().hashCode());
 
         return hashCode;
     }
--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/consumer/server/HttpServerHandler.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/consumer/server/HttpServerHandler.java
@@ -109,6 +109,8 @@ public class HttpServerHandler extends SimpleChannelInboundHandler<FullHttpReque
         httpResponse.headers().set(CONTENT_TYPE, "text/plain; charset=UTF-8");
         httpResponse.headers().setInt( CONTENT_LENGTH, httpResponse.content().writerIndex());
 
+        Thread.sleep(1000);
+
         ChannelFuture future = ctx.writeAndFlush(httpResponse);
 
         if (!HttpUtil.isKeepAlive(fullHttpRequest)) {
